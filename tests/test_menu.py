c8dfed791773a746baf5886656edae1509d5c14d
unknown <srinidhisripathi88@gmail.com>
2026-03-30 22:13:08 +0530

    Fourth commit

--- tests/test_menu.py
+++
@@ -1,5 +0,0 @@
-def test_get_menu(api_client):
-    resp = api_client.get("/menu")
-    assert resp.status_code == 200
-    data = resp.json()
-    assert "items" in data or "menu" in data  # adapt to your actual JSON shape
